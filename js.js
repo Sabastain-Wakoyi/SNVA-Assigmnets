8b39a5edf63de519dcc063861f0a01b838b2b77f
Sabastain Wakoyi <sabastainwakoyi01@gmail.com>
2022-12-30 15:54:20 -0600

    added js

--- js.js
+++ js.js
@@ -2,4 +2,12 @@ function sendInfos(){
     var email = document.getElementById(email).valueOf()
     var phone = document.getElementById(phone).valueOf()
 
+    if(email !== '' && phone !== '') {
+        document.getElementById('two').classList.remove('hidden')
+        document.getElementById('two').classList.add('displayed')
+        document.getElementById('one').classList.remove('displayed')
+        document.getElementById('one').classList.add('hidden')
+    }
+
+
 }
