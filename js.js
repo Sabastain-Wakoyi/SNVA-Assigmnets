98c8371473a1dbe3b79396cca8abf3bb42b5953e
Sabastain Wakoyi <sabastainwakoyi01@gmail.com>
2022-12-30 15:59:41 -0600

    adding js

--- js.js
+++ js.js
@@ -1,12 +1,17 @@
 function sendInfos(){
-    var email = document.getElementById(email).valueOf()
-    var phone = document.getElementById(phone).valueOf()
+    var email = document.getElementById(email).value
+    var phone = document.getElementById(phone).value
 
     if(email !== '' && phone !== '') {
         document.getElementById('two').classList.remove('hidden')
         document.getElementById('two').classList.add('displayed')
         document.getElementById('one').classList.remove('displayed')
         document.getElementById('one').classList.add('hidden')
+
+        document.getElementById(email).value = email
+        document.getElementById(phone).value = phone
+
+
     }
 
 
