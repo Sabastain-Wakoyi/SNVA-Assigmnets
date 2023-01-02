4c85abd83acd5c60d8dc0c10757e07a678df1146
Sabastain Wakoyi <sabastainwakoyi01@gmail.com>
2023-01-02 15:17:52 -0600

    creating table

--- matrix.java
+++ matrix.java
@@ -2,6 +2,10 @@ public class matrix {
     public static void main(String[] args){
         Object[] data = new Object[5];
         data[0] = new Integer[] {1,2,3};
+        data[1] = new Integer[] {4,5,6};
+        data[2] = new Integer[] {7,8,9};
+        data[3] = new Integer[] {10,11,12};
+
 
     }
 }
