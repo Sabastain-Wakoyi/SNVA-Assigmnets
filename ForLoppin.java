0cab7086d703c0bd111033a82bcc70ff2c69671c
Sabastain Wakoyi <sabastainwakoyi01@gmail.com>
2023-01-01 21:47:18 -0600

    java for loop

--- ForLoppin.java
+++ ForLoppin.java
@@ -1,6 +1,7 @@
 public class ForLoppin {
 
     public static void main(String[] args){
-        for(int i = 1; i<=5; i++);
+        for(int i = 1; i<=5; i++)
+            System.out.println("Hello World");
     }
 }
