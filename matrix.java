21104483194efd24c1afcf18bae10147dbda6b65
Sabastain Wakoyi <sabastainwakoyi01@gmail.com>
2023-01-02 15:22:40 -0600

    trying to sort table now

--- matrix.java
+++ matrix.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class matrix {
     public static void main(String[] args){
         Object[] data = new Object[5];
@@ -5,6 +7,12 @@ public class matrix {
         data[1] = new Integer[] {4,5,6};
         data[2] = new Integer[] {7,8,9};
         data[3] = new Integer[] {10,11,12};
+        data[4] = new Integer[] {13,14,15};
+
+        Arrays.sort(data);
+        for (Object row: data){
+
+        }
 
 
     }
