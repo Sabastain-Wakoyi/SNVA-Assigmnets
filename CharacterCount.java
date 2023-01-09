eefdd4fd037b41323a28f455491804fae38dfe69
Sabastain Wakoyi <sabastainwakoyi01@gmail.com>
2023-01-08 21:07:48 -0600

    string character

--- CharacterCount.java
+++ CharacterCount.java
@@ -1,5 +1,11 @@
 public class CharacterCount {
     public static void main(String[] args){
+        int count = 0;
+        for (int i = 0;  i < string.length(); i++){
+            if(string.charA(i) !=" ")
+                count ++;
+        }
+        System.out.println("Total number of characters in a string;" + count);
 
     }
 }
