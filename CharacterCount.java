33e0c039d0d0b85b4f43ee7ea75a0a79eea4db23
Sabastain Wakoyi <sabastainwakoyi01@gmail.com>
2023-01-08 21:11:00 -0600

    string character

--- CharacterCount.java
+++ CharacterCount.java
@@ -1,8 +1,9 @@
 public class CharacterCount {
     public static void main(String[] args){
+        String string = "Essoko Eneteh";
         int count = 0;
-        for (int i = 0;  i < string.length(); i++){
-            if(string.charA(i) !=" ")
+        for (int i = 0; i < string.length(); i++){
+            if(string.charA(i) !='')
                 count ++;
         }
         System.out.println("Total number of characters in a string;" + count);
