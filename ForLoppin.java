6014216bae437bcf92d0d3579bf5ac5c594899cf
Sabastain Wakoyi <sabastainwakoyi01@gmail.com>
2023-01-22 23:15:02 -0600

    testing

--- ForLoppin.java
+++ ForLoppin.java
@@ -1,8 +1,12 @@
 public class ForLoppin {
 
+
     public static void main(String[] args){
         for(int i = 1; i<=5; i++)
             System.out.println("Hello World");
     }
 }
 //java for loop
+
+
+
--- template.js
+++ template.js
@@ -9,3 +9,67 @@ if (value <= MAX) {
 } else {
     console.log(`${value} is greater than ${MAX}`);
 }
+
+/* Tagged template strings */
+function templateHandler(literals, ...values) {
+    var temp = '';
+    for (var i = 0; i < values.length; i++) {
+        temp = temp + literals[i] + values[i];
+    }
+    return temp + literals[values.length];
+}
+
+let firstName = 'Jander', lastName = 'Klander';
+// ${ can be any expression }
+console.log(templateHandler`Hi ${firstName} ${lastName}!`); // String.raw`...`
+// Hi Jander Klander!
+console.log(templateHandler(['Hi ', ' ', '!'], firstName, lastName));
+// Hi Jander Klander!
+
+
+
+
+// Create a function called 'names2Table' which uses string templates to transform:
+// [
+//     { first: 'Jander', last: 'Klander' },
+//     { first: 'Chiquito', last: 'de la Calzada' }
+// ]
+
+
+
+// SOLUTION:
+var names2Table = names => `
+<table> ${names.map(name => `
+  <tr>
+    <td> ${name.first} </td> <td> ${name.last} </td>
+  </tr>
+`)}</table>`.replace(',', '');
+
+console.log(names2Table([
+    {first: 'Jander', last: 'Klander'},
+    {first: 'Chiquito', last: 'de la Calzada'}]
+));
+
+
+
+
+// Factory function 'Symbol' accepting an optional description.
+var symbol1 = Symbol('description');
+var symbol2 = Symbol('description');
+
+console.log(symbol1.toString()); // __$187584550$10$__
+console.log(symbol2.toString()); // __$163428317$11$__
+
+// Symbols are unique.
+console.log(symbol1 === symbol2); // false
+
+// Symbols can be used as property names.
+let obj = {};
+let property = Symbol();
+obj[property] = 'value';
+obj['anotherProperty'] = 'another value';
+
+console.log(obj[property]); // value
+
+console.log(Object.getOwnPropertyNames(obj)); // Ignores symbol-valued keys
+console.log(Object.getOwnPropertySymbols(obj)); // Ignores string-valued keys
